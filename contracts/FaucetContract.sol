0796c99500c7dd705348c04c72a942ed5babd052
Claude Carlsson <claudecarlsson@outlook.com>
2022-12-07 21:29:51 +0100

    Inheritence

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,9 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity = 0.8.17;
 
+import "./owned.sol";
 
-contract Faucet {
-    address[] public funders;
+contract Faucet is Owned{
+
+    uint public numberOfFunders;
+
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
+
+    modifier limitWithdraw(uint withdrawAmount) {
+        require(
+            withdrawAmount <= 100000000000000000, 
+            "Can not withdraw more than one 0.1 ether"
+        );
+        _;
+    }
 
     // Special function 
     // It's called when a txn is made that doesn't specify 
@@ -14,12 +27,43 @@ contract Faucet {
     // txns
     receive() external payable {}
 
-    function addFunds() external payable {
-        funders.push(msg.sender);
+    function transferOwnership(address newOwner) external onlyOwner {
+        owner = newOwner;
     }
 
-    function getAllFunders() external view returns(address[] memory){
-        return funders;
+    function addFunds() external payable {
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            funders[funder] = true;
+            lutFunders[numberOfFunders] = funder;
+            numberOfFunders++;
+        }
+    }
+
+    function test1() external onlyOwner{
+        // Only admin should have access to
+    }
+
+    function test2() external onlyOwner{
+        // Only admin should have access to
+    }
+
+    function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount){
+        payable(msg.sender).transfer(withdrawAmount); 
+    }
+
+    function getAllFunders() external view returns(address[] memory) {
+        address[] memory _funders = new address[](numberOfFunders);
+
+        for (uint i = 0; i < numberOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        return lutFunders[index];
     }
 }
 
@@ -33,5 +77,11 @@ contract Faucet {
 // 8 bytes => 64 bits
 
 
-// instance.addFunds({value: "2000000000000"})
 // const instance = await Faucet.deployed()
+
+// instance.addFunds({from: accounts[0], value: "2000000000000000000"})
+// instance.addFunds({from: accounts[1], value: "2000000000000000000"})
+
+// instance.getFunderAtIndex(0)
+
+// instance.withdraw("500000000000000000", {from: accounts[0]})
